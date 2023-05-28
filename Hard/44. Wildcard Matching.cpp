5c1b6f598a24834a6f16df34ee47a0a630a1693f
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2023-05-28 10:10:15 +0530

    Create 44. Wildcard Matching.cpp

---
+++ Hard/44. Wildcard Matching.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+
+        dp[0][0] = true;
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                
+                if (i == 0 && j == 0)
+                 continue;
+                
+                if (j == 0)
+                 continue;
+
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j-1];
+                    if (i > 0) 
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                    
+                }
+                else if (i > 0 && (p[j - 1] == '?' || s[i-1] == p[j-1]))
+                    dp[i][j] = dp[i-1][j-1];
+                
+            }
+        }
+        return dp[m][n];
+    }
+};
