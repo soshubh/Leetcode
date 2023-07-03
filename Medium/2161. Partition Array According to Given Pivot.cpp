85ebb3e1cb36ebbd52c63514c874cb601b612215
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2023-07-03 15:30:14 +0530

    Create 2161. Partition Array According to Given Pivot.cpp

---
+++ Medium/2161. Partition Array According to Given Pivot.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/partition-array-according-to-given-pivot
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+    vector<int> ans;
+
+    for (const int num : nums)
+      if (num < pivot)
+        ans.push_back(num);
+
+    for (const int num : nums)
+      if (num == pivot)
+        ans.push_back(num);
+
+    for (const int num : nums)
+      if (num > pivot)
+        ans.push_back(num);
+
+    return ans;
+    }
+};
